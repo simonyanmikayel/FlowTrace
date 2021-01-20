0b8f35466b412aafe258e922567b5234ea3f46e8
Michael Simonyan <michael.simonyan.external@worldline.com>
2021-01-20 10:28:38 +0400

    a

--- FlowTrace/MainFrm.cpp
+++ FlowTrace/MainFrm.cpp
@@ -380,7 +380,7 @@ void CMainFrame::SetTitle()
     if (m_importFile.IsEmpty())
         _sntprintf_s(pBuf, _countof(pBuf), _countof(pBuf) - 1, TEXT("%d - %s"), gSettings.GetUdpPort(), TEXT("FlowTrace"));
     else
-        _sntprintf_s(pBuf, _countof(pBuf), _countof(pBuf) - 1, TEXT("%d - %s - %s"), gSettings.GetUdpPort(), m_importFile.GetString(), TEXT("FlowTrace"));
+        _sntprintf_s(pBuf, _countof(pBuf), _countof(pBuf) - 1, TEXT("%s - %s"), m_importFile.GetString(), TEXT("FlowTrace"));
 
     SetWindowText(pBuf);
 }
